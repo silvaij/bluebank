fb3b46c57b33256ae8fdd785d06952a7d660db16
silvaij <isaque.josias@gmail.com>
2021-11-01 10:52:59 -0300

    Primeiro envio configuração do projeto

---
+++ src/test/java/br/com/bloodblue/bluebank/BluebankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bloodblue.bluebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
